bcf44db341eb7f9cbf8905a007c2455133b428f4
pLOPeGG <douzont@gmail.com>
2017-11-15 09:03:57 +0100

    Commit initial : projet IntelliJ avec JFX

---
+++ src/main/java/T_Group/App.java
@@ -0,0 +1,13 @@
+package T_Group;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
