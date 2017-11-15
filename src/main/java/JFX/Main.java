f83a10566127ee6b383e1e5b60f364f63d1ed270
pLOPeGG <douzont@gmail.com>
2017-11-15 21:25:54 +0100

    Gestion JFX avec POC : main JFX ouvre une fenêtre OK

--- src/main/java/JFX/Main.java
+++ src/main/java/JFX/Main.java
@@ -1,9 +1,28 @@
 package JFX;
 
-public class Main {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/POC.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 
+    public static void main(String[] args) {
+        launch(args);
     }
 }
