8a3aac2d6737006ec6007629ce78136f41c70fde
pLOPeGG <douzont@gmail.com>
2017-11-16 13:49:27 +0100

    Premier test Junit (test Addition)

--- src/main/java/T_Group/App.java
+++ src/main/java/T_Group/App.java
@@ -6,6 +6,9 @@ package T_Group;
  */
 public class App 
 {
+    static double addition(double a, double b){
+        return a+b;
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
